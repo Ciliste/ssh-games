d80acd874f7846fc87f975dbbbd4308bcd87aa03
Ciliste <95741289+Ciliste@users.noreply.github.com>
2022-07-01 19:08:26 +0200

    Update Display.java

---
+++ src/main/java/com/sshgames/display/Display.java
@@ -0,0 +1,11 @@
+package display;
+
+/* 
+ * Interface principale d'affichage
+*/
+public interface Display {
+    
+    public abstract void fireScreenUpdate();
+
+    public abstract String getScreen();
+}
