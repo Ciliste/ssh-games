bc711981d0d60d84695ce9704242743ed72c477e
Ciliste <95741289+Ciliste@users.noreply.github.com>
2022-07-02 19:12:00 +0200

    Update TabFrame.java

--- src/main/java/com/sshgames/tab/frame/TabFrame.java
+++ src/main/java/com/sshgames/tab/frame/TabFrame.java
@@ -1,14 +1,63 @@
 package com.sshgames.tab.frame;
 
+/**
+ * Interface qui va gérer les contours d'un onglet 
+ */
 public interface TabFrame {
     
-    public abstract char getUpperLeft ();
+    /**
+     * Retourne le caractère situé en haut à gauche du cadre
+     * 
+     * @return le caractère du coin
+     */
+    public abstract char getUpperLeft();
+
+    /**
+     * Retourne le caractère situé en haut à droite du cadre
+     * 
+     * @return le caractère du coin
+     */
     public abstract char getUpperRight();
-    public abstract char getLowerLeft ();
+
+    /**
+     * Retourne le caractère situé en bas à gauche du cadre
+     * 
+     * @return le caractère du coin
+     */
+    public abstract char getLowerLeft();
+
+    /**
+     * Retourne le caractère situé en bas à droite du cadre
+     * 
+     * @return le caractère du coin
+     */
     public abstract char getLowerRight();
 
+    /**
+     * Retourne la chaîne qui forme le pattern du bord supérieur du cadre
+     * 
+     * @return la chaîne du pattern
+     */
     public abstract String getUpperPattern();
+
+    /**
+     * Retourne la chaîne qui forme le pattern du bord inférieur du cadre
+     * 
+     * @return la chaîne du pattern
+     */
     public abstract String getLowerPattern();
-    public abstract String getLeftPattern ();
+
+    /**
+     * Retourne la chaîne qui forme le pattern du bord gauche du cadre
+     * 
+     * @return la chaîne du pattern
+     */
+    public abstract String getLeftPattern();
+
+    /**
+     * Retourne la chaîne qui forme le pattern du bord droit du cadre
+     * 
+     * @return la chaîne du pattern
+     */
     public abstract String getRightPattern();
 }
