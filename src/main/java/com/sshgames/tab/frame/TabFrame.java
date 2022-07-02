8e11e6214e521352deff29f828fadf04ed131054
Ciliste <95741289+Ciliste@users.noreply.github.com>
2022-07-02 18:48:15 +0200

    Update TabFrame.java

--- src/main/java/com/sshgames/tab/frame/TabFrame.java
+++ src/main/java/com/sshgames/tab/frame/TabFrame.java
@@ -2,4 +2,13 @@ package com.sshgames.tab.frame;
 
 public interface TabFrame {
     
+    public abstract char getUpperLeft ();
+    public abstract char getUpperRight();
+    public abstract char getLowerLeft ();
+    public abstract char getLowerRight();
+
+    public abstract String getUpperPattern();
+    public abstract String getLowerPattern();
+    public abstract String getLeftPattern ();
+    public abstract String getRightPattern();
 }
